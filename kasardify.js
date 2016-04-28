e0d96d0503279796e34e0ed0d09f5109afc54649
John Kasarda <jkasar@saic.edu>
2016-04-28 14:06:08 -0500

    first commit

---
+++ kasardify.js
@@ -0,0 +1,12 @@
+document.body.style.background = "blue";
+// var h = 0;
+// 		function increaseHue(){
+// 			h = h + 1;
+// 			if( h > 360 ){
+// 				h = 0;
+// 			}
+
+// 			$('body').css('background-color','hsl('+h+',100%, 50%)');
+// 		}
+
+// 	document.body.style.background = setInterval( increaseHue, 10 );
